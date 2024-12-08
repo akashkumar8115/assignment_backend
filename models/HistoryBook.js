bcb2757f6989388edd8051e5374f504b01686ee5
akashkumar8115 <akash2884182@gmail.com>
2024-12-08 22:29:12 +0530

    update url

--- models/HistoryBook.js
+++ models/HistoryBook.js
@@ -1,25 +1,32 @@
 const mongoose = require("mongoose");
 
 const historyBookSchema = new mongoose.Schema({
-    bookName: { 
-        type: String, 
-        required: [true, 'Book name is required'] 
+    bookName: {
+        type: String,
+        required: [true, 'Book name is required']
     },
-    authorName: { 
-        type: String, 
-        required: [true, 'Author name is required'] 
+    authorName: {
+        type: String,
+        required: [true, 'Author name is required']
     },
-    price: { 
-        type: Number, 
+    price: {
+        type: Number,
         required: [true, 'Price is required'],
         min: [0, 'Price cannot be negative']
     },
-    imageUrl: { 
+    imageUrl: {
         type: String,
         required: [true, 'Image URL is required'],
-        
+        validate: {
+            validator: function (v) {
+                return /^(http|https):\/\/[^ "]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        },
+        default: "https://www.svgrepo.com/show/94674/books-stack-of-three.svg"
+
     }
-}, { 
+}, {
     timestamps: true,
     versionKey: false
 });
