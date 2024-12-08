21cbbafb4b3071960d5266e9700bfe73f1357acb
akashkumar8115 <akash2884182@gmail.com>
2024-12-08 22:25:04 +0530

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Enhanced CORS configuration
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://assignment-01-three.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
